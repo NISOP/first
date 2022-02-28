4f33ee57ecb3172a5ead3d0056e4a0d99d6ded96
NISOP <sophiafuentes1256@gmail.com>
2022-02-28 07:19:10 -0500

    …

---
+++ Index.js
@@ -0,0 +1,30 @@
+console.log("hola mundo");
+// comentarios en linea 
+/*comentario
+de 
+varias
+lineas*/
+var nombreUsuario; 
+let edadUsuario;
+// declarar variables
+var nombreUsuario;
+// crear variable sin asignar valor
+/* inicializar variable:
+asignar un valor al momento de crear la variable */
+var gradoEstudiante="10A";
+/*asignar un valor: igualar una variable existente a un nuevo valor */
+edadUsuario=16; 
+//asignar valor desde otra variable
+var edad=17;
+edadUsuario=edad;
+console.log(edadUsuario);
+/*asignar valor desde el teclado:
+existen dos maneras de hacerlo la primera es por ventana emergente y la segunda es por medio de un formulario en hmtl
+*/
+var valor=prompt ("ingrese un valor"); 
+console.log ("la variable vale " + valor);
+/*constantes: son valores que no pueden ser cambiados durante la ejecucion del codigo, las constantes siempre dben de ser inicializadas*/
+const PI= 3.1416; 
+/*imprimir por consola*/
+console.log("...");
+console.log(valor)
---
+++ taller 1.js
@@ -0,0 +1,18 @@
+
+/*taller 
+fecha 23/02/2022
+realizado por: isabella agudelo y nicole pianeta*/
+console.log("bailando")
+var nombreEstudiante; //string
+let grado;//double
+let edad;//int
+var grado="10B";
+var nombreEstudiante = "isabella agudelo";
+var edad=prompt ("ingrese su edad");
+console.log (edad);+
+console.log(" el estudiantes "+ nombreEstudiante+"del grado"+ grado +"tiene"+edad)
+
+
+
+
+
